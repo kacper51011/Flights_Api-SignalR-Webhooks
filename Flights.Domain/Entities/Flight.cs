bd6bdc392498d0f238886799caf1851457e6b1de
Kacper <kacper.tylec1999@gmail.com>
2024-02-17 18:20:00 +0100

    Initial Flight model

--- Flights.Domain/Entities/Flight.cs
+++ Flights.Domain/Entities/Flight.cs
@@ -8,5 +8,29 @@ namespace Flights.Domain.Entities
 {
     public class Flight: AggregateRoot
     {
+        private Flight()
+        {
+            FlightId = Guid.NewGuid().ToString();
+
+            
+        }
+        public string FlightId { get; private set; }
+        public StartTime StartTime { get; private set; }
+        public EndTime EndTime { get; private set;}
+
+        public TimeSpan Duration { get; private set;}
+
+        public TimeSpan Delay { get; private set;}
+
+        public string FlyingFrom { get; private set; }
+
+        public string FlyingTo { get; private set; }
+        public bool FlightStarted { get; private set; }
+
+        public bool FlightCompleted { get; private set;}
+
+
     }
+
+
 }
