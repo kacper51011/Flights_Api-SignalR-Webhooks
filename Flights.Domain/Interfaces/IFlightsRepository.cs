aa43258c317e155c0548200054209561fc8f55f1
Kacper <kacper.tylec1999@gmail.com>
2024-02-17 21:07:02 +0100

    IFlightsRepository

---
+++ Flights.Domain/Interfaces/IFlightsRepository.cs
@@ -0,0 +1,22 @@
+﻿using Flights.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flights.Domain.Interfaces
+{
+    public interface IFlightsRepository
+    {
+        public Task<Flight> GetFlightById(string flightId);
+        public Task<List<Flight>> GetCompletedFlights();
+        public Task<List<Flight>> GetStartedFlights();
+        public Task<List<Flight>> GetAllFlights();
+
+        public Task AddFlight(Flight flight);
+        public Task DeleteFlight(Flight flight);
+
+
+    }
+}
