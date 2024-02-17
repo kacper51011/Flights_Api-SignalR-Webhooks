127ea41d49404d7e1f0aa796381be9d5817b639d
Kacper <kacper.tylec1999@gmail.com>
2024-02-17 18:25:32 +0100

    DDD behaviours in aggregate root

--- Flights.Domain/AggregateRoot.cs
+++ Flights.Domain/AggregateRoot.cs
@@ -10,7 +10,30 @@ namespace Flights.Domain
     {
         public string Id { get; private set; }
         public DateTime CreatedAt { get; private set; }
+        public DateTime ViewedAt { get; private set; }
         public DateTime UpdatedAt { get; private set; }
         public int Version { get; private set; }
+
+
+        public void InitializeRoot()
+        { 
+            CreatedAt = DateTime.UtcNow;
+            UpdatedAt = DateTime.UtcNow;
+            ViewedAt = DateTime.UtcNow;
+            Version = 0;
+        }
+        public void IncrementVersion ()
+        {
+            Version++;
+            UpdatedAt = DateTime.UtcNow;
+            ViewedAt = DateTime.UtcNow;
+
+        }
+        public void SetViewedDate()
+        {
+            ViewedAt = DateTime.UtcNow;
+        }
+
+
     }
 }
