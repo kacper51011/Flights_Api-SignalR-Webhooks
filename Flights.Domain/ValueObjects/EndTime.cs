ac26fc819712607c5f5100ac33ff43487d686c37
Kacper <kacper.tylec1999@gmail.com>
2024-02-17 18:49:01 +0100

    EndTime

--- Flights.Domain/ValueObjects/EndTime.cs
+++ Flights.Domain/ValueObjects/EndTime.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Flights.Domain.Exceptions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,5 +11,20 @@ namespace Flights.Domain.ValueObjects
     public class EndTime
     {
         public DateTime Value { get; private set; }
+
+        public EndTime(StartTime startTime, DateTime endTime)
+        {
+            Validate(startTime, endTime);
+
+            Value = endTime;
+        }
+
+        private void Validate(StartTime startTime, DateTime endTime)
+        {
+            if(endTime < startTime.Value)
+            {
+                throw new DomainException("End of the flight must be later than start");
+            }
+        }
     }
 }
