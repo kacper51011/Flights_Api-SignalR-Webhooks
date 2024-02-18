a377c33ccde28d721c0a9b2aa4cf952aa9a54e14
Kacper <kacper.tylec1999@gmail.com>
2024-02-18 13:51:21 +0100

    Initial Webhook Subscription model

---
+++ Flights.Domain/Models/WebhookSubscription.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flights.Domain.Models
+{
+    public class WebhookSubscription
+    {
+        public string Id { get; set; }
+        public string WebhookUri { get; set; }
+        public string Secret { get; set; }
+
+        public string WebhookType { get; set; }
+        public string Publisher { get; set; }
+
+    }
+}
---
+++ Flights.Infrastructure/Db/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Flights.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flights.Infrastructure.Db
+{
+    public class ApplicationDbContext: DbContext
+    {
+        DbSet<Flight> Flights { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+            var dbCreator = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
+            if (dbCreator != null)
+            {
+                if (!dbCreator.CanConnect()) dbCreator.Create();
+                if (!dbCreator.HasTables()) dbCreator.CreateTables();
+            }
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
+        }
+    }
+}
