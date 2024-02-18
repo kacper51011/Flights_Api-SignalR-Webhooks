16b7b788c4f953fd065133de5208df1ce626db7b
Kacper <kacper.tylec1999@gmail.com>
2024-02-18 14:07:49 +0100

    Unit of work

---
+++ Flights.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using Flights.Domain.Interfaces;
+using Flights.Infrastructure.Db;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flights.Infrastructure.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
