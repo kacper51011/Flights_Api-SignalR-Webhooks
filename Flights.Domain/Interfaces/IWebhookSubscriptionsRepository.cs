1e5e8d7a5be44e0a03345651f22f6e2b0ac9ccc9
Kacper <kacper.tylec1999@gmail.com>
2024-02-18 13:53:25 +0100

    webhookSubscriptions repository interface

---
+++ Flights.Domain/Interfaces/IWebhookSubscriptionsRepository.cs
@@ -0,0 +1,17 @@
+﻿using Flights.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flights.Domain.Interfaces
+{
+    public interface IWebhookSubscriptionsRepository
+    {
+        public Task<WebhookSubscription> GetSubscription(string subscriptionId);
+        public Task<WebhookSubscription> GetSubscriptionByUri(string uri);
+        public Task<List<WebhookSubscription>> GetAllSubscriptions();
+        public Task AddSubscription(WebhookSubscription subscription);
+    }
+}
