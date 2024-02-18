d3dc4805613cc59cc037ff239f49ef14d1d377f8
Kacper <kacper.tylec1999@gmail.com>
2024-02-18 23:29:40 +0100

    IncrementFlightDelayCommandHandler

---
+++ Flights.Application/Commands/IncrementFlightDelay/IncrementFlightDelayCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using Flights.Domain.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flights.Application.Commands.IncrementFlightDelay
+{
+    public class IncrementFlightDelayCommandHandler : IRequestHandler<IncrementFlightDelayCommand>
+    {
+        private readonly IFlightsRepository _repository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public async Task Handle(IncrementFlightDelayCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var flight = await _repository.GetFlightById(request.id);
+                if(flight == null)
+                {
+                    throw new FileNotFoundException();
+                }
+                flight.IncrementDelay(request.incrementDelayValue);
+
+                await _unitOfWork.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
