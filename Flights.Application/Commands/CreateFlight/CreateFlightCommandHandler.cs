14e00e5db313f201c5fe2e1c373df80f5faef479
Kacper <kacper.tylec1999@gmail.com>
2024-02-19 19:57:35 +0100

    CreateFlightCommandHandler

--- Flights.Application/Commands/CreateFlight/CreateFlightCommandHandler.cs
+++ Flights.Application/Commands/CreateFlight/CreateFlightCommandHandler.cs
@@ -1,4 +1,7 @@
-﻿using MediatR;
+﻿using Flights.Domain.Entities;
+using Flights.Domain.Interfaces;
+using Flights.Domain.ValueObjects;
+using MediatR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +12,32 @@ namespace Flights.Application.Commands.CreateFlight
 {
     public class CreateFlightCommandHandler : IRequestHandler<CreateFlightCommand, string>
     {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IFlightsRepository _flightsRepository;
+
+        public CreateFlightCommandHandler(IUnitOfWork unitOfWork, IFlightsRepository flightsRepository)
+        {
+            _unitOfWork = unitOfWork;
+            _flightsRepository = flightsRepository;
+            
+        }
         public async Task<string> Handle(CreateFlightCommand request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            //there can be 2 flights to the same place at the same time so i won`t check do flight already exist
+            try
+            {
+
+                var flight = Flight.Create(request.dto.StartTime, request.dto.EndTime, request.dto.From, request.dto.To);
+                await _flightsRepository.AddFlight(flight);
+                await _unitOfWork.SaveChangesAsync();
+
+                return flight.FlightId;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
     }
 }
