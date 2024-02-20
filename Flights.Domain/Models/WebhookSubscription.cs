37c70383a36face480637714f5043d04574bac08
Kacper <kacper.tylec1999@gmail.com>
2024-02-20 01:06:41 +0100

    AlreadyExist exception, createSubscriptionDto

--- Flights.Domain/Models/WebhookSubscription.cs
+++ Flights.Domain/Models/WebhookSubscription.cs
@@ -8,12 +8,16 @@ namespace Flights.Domain.Models
 {
     public class WebhookSubscription
     {
+        public WebhookSubscription(string webhookUri)
+        {
+            Id = Guid.NewGuid().ToString();
+            WebhookUri = webhookUri;
+            // secret is in a form of guid only to simplify the dataflow
+            Secret = Guid.NewGuid().ToString();
+        }
         public string Id { get; set; }
         public string WebhookUri { get; set; }
         public string Secret { get; set; }
-
-        public string WebhookType { get; set; }
-        public string Publisher { get; set; }
 
     }
 }
