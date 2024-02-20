eb0394df30495f1f99e2d3633d204791276bc6f7
Kacper <kacper.tylec1999@gmail.com>
2024-02-20 01:02:31 +0100

    Create subscription command

---
+++ Flights.Application/Commands/CreateSubscription/CreateSubscriptionCommand.cs
@@ -0,0 +1,14 @@
+﻿using Flights.Application.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flights.Application.Commands.CreateSubscription
+{
+    public record CreateSubscriptionCommand(CreateSubscriptionDto createSubscriptionDto): IRequest<string>
+    {
+    }
+}
---
+++ Flights.Application/Commands/CreateSubscription/CreateSubscriptionCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using Flights.Application.Exceptions;
+using Flights.Domain.Interfaces;
+using Flights.Domain.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flights.Application.Commands.CreateSubscription
+{
+    public class CreateSubscriptionCommandHandler : IRequestHandler<CreateSubscriptionCommand, string>
+    {
+        private readonly IWebhookSubscriptionsRepository _webhookSubscriptionsRepository;
+        public async Task<string> Handle(CreateSubscriptionCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var response = await _webhookSubscriptionsRepository.GetSubscriptionByUri(request.createSubscriptionDto.WebhookUri);
+                if (response != null)
+                {
+                    throw new AlreadyExistsException("Subscription for that uri already exists!");
+                }
+
+                WebhookSubscription subscription = new(request.createSubscriptionDto.WebhookUri);
+                await _webhookSubscriptionsRepository.AddSubscription(subscription);
+
+                return subscription.Secret;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
