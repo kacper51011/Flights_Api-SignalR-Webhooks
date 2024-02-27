fa19d87189c352a7093bf42c1316e9b8c79710d6
Kacper <kacper.tylec1999@gmail.com>
2024-02-27 16:01:28 +0100

    Separate flight validation logic for domain model

--- Flights/Flights.Domain/Validations/FlightValidations.cs
+++ Flights/Flights.Domain/Validations/FlightValidations.cs
@@ -1,16 +1,11 @@
 ﻿using Flights.Domain.Entities;
 using Flights.Domain.Exceptions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Flights.Domain.Validations
 {
     public static class FlightValidations
     {
-        public static void Validate(this Flight flight, DateTime startTime, DateTime endTime, string from, string to)
+        public static void ValidateCreation(this Flight flight, DateTime startTime, DateTime endTime, string from, string to)
         {
             //Due to the limitations of Entity Framework Core, I had to give up validation in valuable objects
             if (endTime < startTime)
@@ -26,10 +21,6 @@ namespace Flights.Domain.Validations
             {
                 throw new DomainException("Can`t add Flight which already started");
             }
-            if (String.Equals(from, to, StringComparison.OrdinalIgnoreCase))
-            {
-                throw new DomainException("Can`t create flight with the same destination");
-            }
         }
 
         public static void ValidateIncrementDelay(this Flight flight)
@@ -39,9 +30,18 @@ namespace Flights.Domain.Validations
                 throw new DomainException("Can`t increment of already completed flight");
             }
         }
-        public void ValidateDecrementDelay(this Flight flight, TimeSpan delayDecrementedByValue)
+        public static void ValidateDecrementDelay(this Flight flight, TimeSpan delayDecrementedByValue)
         {
-
+            if (flight.FlightCompleted)
+            {
+                throw new DomainException("Can`t increment of already completed flight");
+            }
+            if(flight.Delay - delayDecrementedByValue < TimeSpan.Zero)
+            {
+                throw new DomainException("Flight delay cannot be less than 0 seconds!");
+            }
         }
+
+
     }
 }
