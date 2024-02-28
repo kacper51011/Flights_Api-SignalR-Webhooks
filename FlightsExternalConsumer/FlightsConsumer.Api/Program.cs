09792bd73ba39bae1cfd57d8117b02e91941ab3e
Kacper <kacper.tylec1999@gmail.com>
2024-02-28 08:59:47 +0100

    FlightsHub init

--- FlightsExternalConsumer/FlightsConsumer.Api/Program.cs
+++ FlightsExternalConsumer/FlightsConsumer.Api/Program.cs
@@ -1,3 +1,5 @@
+using FlightsConsumer.Application.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -20,6 +22,7 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.MapHub<FlightsHub>("/flightshub");
 app.MapControllers();
 
 app.Run();
---
+++ FlightsExternalConsumer/FlightsConsumer.Application/Hubs/FlightsHub.cs
@@ -0,0 +1,23 @@
+﻿using FlightsConsumer.Domain.Interfaces;
+using Microsoft.AspNetCore.SignalR;
+
+namespace FlightsConsumer.Application.Hubs
+{
+    public class FlightsHub : Hub
+    {
+        private readonly IFlightsRepository _flightsRepository;
+        public FlightsHub(IFlightsRepository flightsRepository)
+        {
+
+            _flightsRepository = flightsRepository;
+
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("ReceiveMessage", $"{Context.ConnectionId} has joined");
+
+        }
+
+    }
+}
