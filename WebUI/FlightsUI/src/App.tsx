e5bb4d745fa120b2f6b844707f6d03087c00f173
Kacper <kacper.tylec1999@gmail.com>
2024-03-01 16:35:13 +0100

    handling signalr methods from client side

--- WebUI/FlightsUI/src/App.tsx
+++ WebUI/FlightsUI/src/App.tsx
@@ -1,20 +1,7 @@
-import { useState } from "react";
 import "./App.css";
-import { HubConnectionBuilder } from "@microsoft/signalr";
-import { ItemLetter } from "./Components/ItemLetter";
 import { FlightList } from "./Components/FlightList";
 
 function App() {
-  // const [connection, setConnection] = useState(null);
-
-  // const connectToServer = async () => {
-  //   try {
-  //     const conn = new HubConnectionBuilder().withUrl("https://localhost:8005").build();
-
-  //     conn.on("InitializedConnection", (flights: string) => {});
-  //   } catch (error) {}
-  // };
-
   return (
     <div className="w-full h-screen flex flex-col items-center bg-gray-900">
       <h1 className="font-bold text-3xl text-yellow-500 font-serif m-4">Current Flights</h1>
--- WebUI/FlightsUI/src/Components/FlightList.tsx
+++ WebUI/FlightsUI/src/Components/FlightList.tsx
@@ -1,8 +1,55 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FlightItem } from "./FlightItem";
+import { HubConnection, HubConnectionBuilder } from "@microsoft/signalr";
+
+type Flight = {
+  flightId: string;
+  letters: string;
+};
 
 export const FlightList = () => {
   const [placeholder, setPlaceholder] = useState("Warsaw    Warsaw    14261528start 14");
+  const [connection, setConnection] = useState<HubConnection | null>(null);
+  const [flights, setFlights] = useState<Flight[]>([]);
+
+  useEffect(() => {
+    const conn = new HubConnectionBuilder()
+      .withUrl("https://localhost:8001/flightshub")
+      .withAutomaticReconnect()
+      .build();
+
+    setConnection(conn);
+  }, []);
+
+  useEffect(() => {
+    if (connection) {
+      connection.start().then(() => {
+        console.log("connected to hub");
+      });
+
+      connection.on("Initialize", (flightList: Flight[]) => {
+        setFlights(flightList);
+      });
+
+      connection.on("ModifyFlight", (modifiedFlight: Flight) => {
+        setFlights((prevFlights) =>
+          prevFlights.map((x) => (x.flightId == modifiedFlight.flightId ? modifiedFlight : x))
+        );
+      });
+
+      connection.on("DeleteFlight", (deletedFlightId: string) => {
+        setFlights((prevFlights) => prevFlights.filter((x) => x.flightId != deletedFlightId));
+      });
+
+      connection.on("AddFlight", (flightToAdd: Flight) => {
+        setFlights((prevFlights) => [...prevFlights, flightToAdd]);
+      });
+    }
+    return () => {
+      connection?.stop();
+    };
+  }, [connection]);
+
   return (
     <main className=" bg-gray-700 rounded-xl p-2 font-bold text-xl text-yellow-500">
       <ul className="flex flex-row ">
