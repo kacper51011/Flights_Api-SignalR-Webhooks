a09aeeb62081c5f3cab60930f026db3e7e817f46
Kacper <kacper.tylec1999@gmail.com>
2024-03-01 19:13:32 +0100

    Messages for MT RBMQ in flightConsumer

---
+++ FlightsExternalConsumer/FlightsConsumer.Application/Messages/FlightUpdated.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightsConsumer.Application.Messages
+{
+    public record FlightUpdated
+    {
+        public string FlightId { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        public TimeSpan Duration { get; set; }
+
+        public TimeSpan Delay { get; set; }
+
+        public string From { get; set; }
+
+        public string To { get; set; }
+        public bool FlightStarted { get; set; }
+        public bool FlightCompleted { get; set; }
+        public DateTime DateOfUpdate { get; set; }
+    }
+}
