2bddb41d8c3ebea605704687a232cc26356e5bcb
Kacper <kacper.tylec1999@gmail.com>
2024-03-01 19:14:17 +0100

    FlightsSignalRDto flight extension method for mapping

--- FlightsExternalConsumer/FlightsConsumer.Application/Dtos/FlightSignalRDto.cs
+++ FlightsExternalConsumer/FlightsConsumer.Application/Dtos/FlightSignalRDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FlightsConsumer.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,8 +8,49 @@ using System.Threading.Tasks;
 namespace FlightsConsumer.Application.Dtos
 {
     public class FlightSignalRDto
-    {
+    {     
         public string Id {  get; set; }
-        public string formatedValue { get; set; }
+        public string FormatedValue { get; set; }
     }
+
+
+    public static class FlightsExtensions
+    {
+        public static FlightSignalRDto ToSignalRDto(this Flight flight)
+        {
+            var flightDto = new FlightSignalRDto();
+            flightDto.Id = flight.FlightId;
+
+            var fromStringArray = new Char[10];
+            var toStringArray = new Char[10];
+
+            for (int i = 0; i < fromStringArray.Length; i++) { fromStringArray[i] = flight.From[i]; }
+            for (int i = 0; i < toStringArray.Length; i++) { toStringArray[i] = flight.To[i]; }
+
+            var formattedStart = flight.StartTime.Minute.ToString() + flight.StartTime.Second.ToString();
+            var formattedEnd = flight.EndTime.Minute.ToString() + flight.EndTime.Second.ToString();
+
+            string currentSituation;
+            if (flight.FlightCompleted)
+            {
+                currentSituation = "Ended";
+            }
+            else if (flight.FlightStarted)
+            {
+                currentSituation = "Started";
+            }
+            else
+            {
+                currentSituation = "Wait";
+            }
+
+            var formattedDelay = flight.Delay.TotalMinutes;
+
+            flightDto.FormatedValue = new string(fromStringArray) + new string(toStringArray) + formattedStart + formattedEnd + currentSituation + formattedDelay;
+
+            return flightDto;
+        }
+    }
+
+
 }
--- FlightsExternalConsumer/FlightsConsumer.Application/Hubs/FlightsHub.cs
+++ FlightsExternalConsumer/FlightsConsumer.Application/Hubs/FlightsHub.cs
@@ -21,45 +21,18 @@ namespace FlightsConsumer.Application.Hubs
 
             foreach (var flight in flights)
             {
-                var flightDto = new FlightSignalRDto();
-                flightDto.Id = flight.FlightId;
-
-                var fromStringArray = new Char[10];
-                var toStringArray = new Char[10];
-
-                for (int i = 0; i < fromStringArray.Length; i++) { fromStringArray[i] = flight.From[i]; }
-                for (int i = 0; i < toStringArray.Length; i++) { toStringArray[i] = flight.To[i]; }
-
-                var formattedStart = flight.StartTime.Minute.ToString() + flight.StartTime.Second.ToString();
-                var formattedEnd = flight.EndTime.Minute.ToString() + flight.EndTime.Second.ToString();
-
-                string currentSituation;
-                if (flight.FlightCompleted )
-                {
-                    currentSituation = "Ended";
-                }
-                else if (flight.FlightStarted)
-                {
-                    currentSituation = "Started";
-                }
-                else
-                {
-                    currentSituation = "Wait";
-                }
-
-                var formattedDelay = flight.Delay.TotalMinutes;
-
-                flightDto.formatedValue = new string(fromStringArray) + new string(toStringArray) + formattedStart + formattedEnd + currentSituation + formattedDelay;
+                // created through extension method because its a bit complex mapping
+                var flightDto = flight.ToSignalRDto();
                 
                 flightsDtos.Add(flightDto);
             }
-
+                await Clients.All.SendAsync("Initialize", flightsDtos);
         }
 
     }
 
 
-        //await Clients.All.SendAsync("ReceiveMessage", $"{Context.ConnectionId} has joined");
+
 
     }
 
